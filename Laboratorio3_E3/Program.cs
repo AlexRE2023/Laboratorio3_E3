3b53cb9d331ee05e8fc13b55d52956a249c95c34
alexi <alexi@ALEXANDER>
2023-09-05 00:03:16 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio3_E3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Ingrese la cantidad de adivinanzas acertadas:");
+int aciertos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la edad del participante:");
+int edad = int.Parse(Console.ReadLine());
+
+switch (aciertos)
+{
+    case < 3:
+        switch (edad)
+        {
+            case < 18:
+                int montofinal = 50 + 5 * edad + 2 * aciertos;
+                Console.WriteLine("El premio final es de " + montofinal + " soles");
+                break;
+            case >= 18:
+                montofinal = 30 + 5 * edad + 2 * aciertos;
+                Console.WriteLine("El premio final es de " + montofinal + " soles");
+                break;
+        }
+        break;
+    case <= 5:
+        switch (edad)
+        {
+            case < 18:
+                int montofinal = 80 + 5 * edad + 2 * aciertos;
+                Console.WriteLine("El premio final es de " + montofinal + " soles");
+                break;
+            case >= 18:
+                montofinal = 50 + 5 * edad + 2 * aciertos;
+                Console.WriteLine("El premio final es de " + montofinal + " soles");
+                break;
+        }
+        break;
+}
